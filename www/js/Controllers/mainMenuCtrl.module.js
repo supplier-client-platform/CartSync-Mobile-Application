7959a61b21e9626ae64cbbff0e5750104ea4af66
Nipuna <nipuna@app-monkeyz.com>
2016-10-17 00:24:02 +0530

    Added user's email to order array

--- www/js/Controllers/mainMenuCtrl.module.js
+++ www/js/Controllers/mainMenuCtrl.module.js
@@ -1,73 +1,62 @@
 angular.module('app.mainMenucontroller', []).controller('mainMenuCtrl', function($scope, $firebase, $restClient, $rootScope, $ionicSideMenuDelegate, fireBaseData, $state, $ionicHistory, $firebaseArray, sharedCartService, sharedUtils) {
-    //Check if user already logged in
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            $scope.user_info = user; //Saves data to user_info
-        } else {
-            $ionicSideMenuDelegate.toggleLeft(); //To close the side bar
-            $ionicSideMenuDelegate.canDragContent(false); // To remove the sidemenu white space
-            $ionicHistory.nextViewOptions({
-                historyRoot: true
+            //Check if user already logged in
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    $scope.user_info = user; //Saves data to user_info
+                } else {
+                    $ionicSideMenuDelegate.toggleLeft(); //To close the side bar
+                    $ionicSideMenuDelegate.canDragContent(false); // To remove the sidemenu white space
+                    $ionicHistory.nextViewOptions({
+                        historyRoot: true
+                    });
+                    $rootScope.extras = false;
+                    sharedUtils.hideLoading();
+                    $state.go('tabsController.login', {}, {
+                        location: "replace"
+                    });
+                }
             });
-            $rootScope.extras = false;
-            sharedUtils.hideLoading();
-            $state.go('tabsController.login', {}, {
-                location: "replace"
+            // On Loggin in to menu page, the sideMenu drag state is set to true
+            $ionicSideMenuDelegate.canDragContent(true);
+            $rootScope.extras = true;
+            // When user visits A-> B -> C -> A and clicks back, he will close the app instead of back linking
+            $scope.$on('$ionicView.enter', function(ev) {
+                if (ev.targetScope !== $scope) {
+                    $ionicHistory.clearHistory();
+                    $ionicHistory.clearCache();
+                }
             });
-        }
-    });
-    // On Loggin in to menu page, the sideMenu drag state is set to true
-    $ionicSideMenuDelegate.canDragContent(true);
-    $rootScope.extras = true;
-    // When user visits A-> B -> C -> A and clicks back, he will close the app instead of back linking
-    $scope.$on('$ionicView.enter', function(ev) {
-        if (ev.targetScope !== $scope) {
-            $ionicHistory.clearHistory();
-            $ionicHistory.clearCache();
-        }
-    });
 
-    $scope.isActive = function() {
-        return true;
-    };
+            $scope.isActive = function() {
+                return true;
+            };
 
-    $scope.loadMenu = function() {
-        $scope.onlyNumbers = /^\d+$/;
-        $restClient.getProducts(function(msg) {
-            console.log(JSON.stringify(msg));
-            $scope.menu = msg.products;
-        });
-    }
+            $scope.loadMenu = function() {
+                $scope.onlyNumbers = /^\d+$/;
+                $restClient.getProducts(function(msg) {
+                    console.log(JSON.stringify(msg));
+                    $scope.menu = msg.products;
+                });
+            }
 
-    $scope.addToCart = function(a) {
-        // console.log(JSON.stringify($scope.menu[a]));
-        $rootScope.cartList.push({
-            qty: "0",
-            item: $scope.menu[a]
-        });
+            $scope.addToCart = function(a) {
 
-        console.log(JSON.stringify($rootScope.cartList));
-    }
+                console.log("UserInfo " + $scope.user_info.email);
 
-    $scope.showProductInfo = function(id) {};
+                    $rootScope.cartList.push({
+                        qty: "0",
+                        item: $scope.menu[a]
+                    });
 
-    $scope.getUserDetails = function() {
-        $restClient.getUserDetails(function(msg) {
-            console.log(JSON.stringify(msg));
-        });
-    };
+                    console.log(JSON.stringify($rootScope.cartList));
+                }
 
-    $scope.getPost = function() {
-        // $restClient.getPost(1,function(msg){
-        //     console.log(JSON.stringify(msg));
-        // });
+                $scope.showProductInfo = function(id) {};
 
-        $scope.auth = $firebaseArray(fireBaseData.ref());
-        // $scope.menu.$loaded().then(function (items) {
-        // $scope.menu = items;
-        // console.log('=========>' + JSON.stringify(items)); // populated array
-        // });
+                $scope.getUserDetails = function() {
+                    $restClient.getUserDetails(function(msg) {
+                        console.log(JSON.stringify(msg));
+                    });
+                };
 
-    };
-
-})
+            })
--- www/js/services.js
+++ www/js/services.js
@@ -1,37 +1,37 @@
-angular.module('app.services', []).factory('fireBaseData', function ($firebase, $firebaseArray) {
-    var ref = new Firebase("https://sepii-client-app.firebaseio.com/")
-        , refCart = new Firebase("https://sepii-client-app.firebaseio.com/cart")
-        , refUser = new Firebase("https://sepii-client-app.firebaseio.com/users")
-        , refCategory = new Firebase("https://sepii-client-app.firebaseio.com/category")
-        , refOrder = new Firebase("https://sepii-client-app.firebaseio.com/orders")
-        , refFeatured = new Firebase("https://sepii-client-app.firebaseio.com/featured")
-        , refMenu = new Firebase("https://sepii-client-app.firebaseio.com/menu");
+angular.module('app.services', []).factory('fireBaseData', function($firebase, $firebaseArray) {
+    var ref = new Firebase("https://sepii-client-app.firebaseio.com/"),
+        refCart = new Firebase("https://sepii-client-app.firebaseio.com/cart"),
+        refUser = new Firebase("https://sepii-client-app.firebaseio.com/users"),
+        refCategory = new Firebase("https://sepii-client-app.firebaseio.com/category"),
+        refOrder = new Firebase("https://sepii-client-app.firebaseio.com/orders"),
+        refFeatured = new Firebase("https://sepii-client-app.firebaseio.com/featured"),
+        refMenu = new Firebase("https://sepii-client-app.firebaseio.com/menu");
     return {
-        ref: function () {
+        ref: function() {
             return ref;
-        }
-        , refCart: function () {
+        },
+        refCart: function() {
             return refCart;
-        }
-        , refUser: function () {
+        },
+        refUser: function() {
             return refUser;
-        }
-        , refCategory: function () {
+        },
+        refCategory: function() {
             return refCategory;
-        }
-        , refOrder: function () {
+        },
+        refOrder: function() {
             return refOrder;
-        }
-        , refFeatured: function () {
+        },
+        refFeatured: function() {
             return refFeatured;
-        }
-        , refMenu: function () {
+        },
+        refMenu: function() {
             return refMenu;
         }
     }
-}).factory('sharedUtils', ['$ionicLoading', '$ionicPopup', function ($ionicLoading, $ionicPopup) {
+}).factory('sharedUtils', ['$ionicLoading', '$ionicPopup', function($ionicLoading, $ionicPopup) {
     var functionObj = {};
-    functionObj.showLoading = function () {
+    functionObj.showLoading = function() {
         $ionicLoading.show({
             content: '<i class=" ion-loading-c"></i> ', // The text to display in the loading indicator
             animation: 'fade-in', // The animation to use
@@ -40,84 +40,80 @@ angular.module('app.services', []).factory('fireBaseData', function ($firebase, 
             showDelay: 0 // The delay in showing the indicator
         });
     };
-    functionObj.hideLoading = function () {
+    functionObj.hideLoading = function() {
         $ionicLoading.hide();
     };
-    functionObj.showAlert = function (title, message) {
+    functionObj.showAlert = function(title, message) {
         var alertPopup = $ionicPopup.alert({
-            title: title
-            , template: message
+            title: title,
+            template: message
         });
     };
     return functionObj;
-}]).factory('sharedCartService', ['$ionicPopup', 'fireBaseData', '$firebaseArray', function ($ionicPopup, fireBaseData, $firebaseArray) {
+}]).factory('sharedCartService', ['$ionicPopup', 'fireBaseData', '$firebaseArray', function($ionicPopup, fireBaseData, $firebaseArray) {
     var uid; // uid is temporary user_id
     var cart = {}; // the main Object
     //Check if user already logged in
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             uid = user.uid;
             cart.cart_items = $firebaseArray(fireBaseData.refCart().child(uid));
         }
     });
     //Add to Cart
-    cart.add = function (item) {
+    cart.add = function(item) {
         //check if item is already added or not
-        fireBaseData.refCart().child(uid).once("value", function (snapshot) {
+        fireBaseData.refCart().child(uid).once("value", function(snapshot) {
             if (snapshot.hasChild(item.$id) == true) {
                 //if item is already in the cart
                 var currentQty = snapshot.child(item.$id).val().item_qty;
                 fireBaseData.refCart().child(uid).child(item.$id).update({ // update
                     item_qty: currentQty + 1
                 });
-            }
-            else {
+            } else {
                 //if item is new in the cart
                 fireBaseData.refCart().child(uid).child(item.$id).set({ // set
-                    item_name: item.name
-                    , item_image: item.image
-                    , item_price: item.price
-                    , item_qty: 1
+                    item_name: item.name,
+                    item_image: item.image,
+                    item_price: item.price,
+                    item_qty: 1
                 });
             }
         });
     };
-    cart.drop = function (item_id) {
+    cart.drop = function(item_id) {
         fireBaseData.refCart().child(uid).child(item_id).remove();
     };
-    cart.increment = function (item_id) {
+    cart.increment = function(item_id) {
         //check if item is exist in the cart or not
-        fireBaseData.refCart().child(uid).once("value", function (snapshot) {
+        fireBaseData.refCart().child(uid).once("value", function(snapshot) {
             if (snapshot.hasChild(item_id) == true) {
                 var currentQty = snapshot.child(item_id).val().item_qty;
                 //check if currentQty+1 is less than available stock
                 fireBaseData.refCart().child(uid).child(item_id).update({
                     item_qty: currentQty + 1
                 });
-            }
-            else {
+            } else {
                 //pop error
             }
         });
     };
-    cart.decrement = function (item_id) {
+    cart.decrement = function(item_id) {
         //check if item is exist in the cart or not
-        fireBaseData.refCart().child(uid).once("value", function (snapshot) {
+        fireBaseData.refCart().child(uid).once("value", function(snapshot) {
             if (snapshot.hasChild(item_id) == true) {
                 var currentQty = snapshot.child(item_id).val().item_qty;
                 if (currentQty - 1 <= 0) {
                     cart.drop(item_id);
-                }
-                else {
+                } else {
                     fireBaseData.refCart().child(uid).child(item_id).update({
                         item_qty: currentQty - 1
                     });
                 }
-            }
-            else {
+            } else {
                 //pop error
             }
         });
     };
     return cart;
-  }]).factory('BlankFactory', [function () {}]).service('BlankService', [function () {}]);
+}]).factory('BlankFactory', [function() {}]).service('BlankService', [function() {}]);
